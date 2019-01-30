01be20844d0349ed3273edb2a8e6c83989c4d3a3
yuans <276948327@qq.com>
2019-01-30 15:22:10 +0800

    Update Hello.java File . Add Two Lines

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
 	public static void main (String args[]){
-		System.out.println("Hello World !");
+		System.out.println("Hellow Yootk .");
+		System.out.println("www.yootk.com");
 	}
 }
