0a3cc88d4be12ad92ba4f9e292a376c3767fad5e
yuans <276948327@qq.com>
2019-02-15 14:39:12 +0800

    change two Files

--- Demo.java
+++ Demo.java
@@ -1,5 +1,5 @@
 public class Demo{
 	public static void main (String args[]){
-		System.out.println("Current Date = " + new java.util.Date());
+		System.out.println("1111Current Date = " + new java.util.Date());
 	}
 }
