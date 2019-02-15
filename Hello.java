5051d9753acd658ce6b610f2f2134e304b421dcd
yuans <276948327@qq.com>
2019-02-15 16:04:57 +0800

    Modified Hello.java Files

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello{
 	public static void main (String args[]){
-		System.out.println("yootk1");
+		System.out.println("Happy New Year");
+		System.out.println("Happy MySelf");
 	}
 }
